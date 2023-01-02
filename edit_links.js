3f74f35c8cfa884b9ef5bfc33330961075fb2c5d
thenamesjagger <jaggerwarren@gmail.com>
2023-01-01 20:05:47 -0800

    edit_links add and delete

--- edit_links.js
+++ edit_links.js
@@ -1,18 +1,17 @@
 window.onload = function() {
 
-    const container = document.querySelector('#container');
+  const container = document.querySelector('#container');
 
-    // Retrieve current data from local storage
-    const one = JSON.parse(localStorage.getItem('one'));
-    const two = JSON.parse(localStorage.getItem('two'));
-    const three = JSON.parse(localStorage.getItem('three'));
+  // Retrieve current data from local storage
+  let one = JSON.parse(localStorage.getItem('one'));
+  let two = JSON.parse(localStorage.getItem('two'));
+  let three = JSON.parse(localStorage.getItem('three'));
 
+  // Create a table to display the data
+  const table = document.createElement('table');
 
-    // Create a table to display the data
-    const table = document.createElement('table');
-
-    // Create a row for each item in the arrays
-    for (let i = 0; i < 3; i++) {
+  // Create a row for each item in the arrays
+  one.forEach((item, i) => {
     const tr = document.createElement('tr');
 
     // Create a cell for each item
@@ -26,38 +25,37 @@ window.onload = function() {
     // Create a form for each item
     const formOne = document.createElement('form');
     formOne.addEventListener('submit', (event) => {
-        event.preventDefault();
+      event.preventDefault();
 
-        // Update the item's name and link
-        one[i].name = event.target[0].value;
-        one[i].link = event.target[1].value;
+      // Update the item's name and link
+      one[i].name = event.target[0].value;
+      one[i].link = event.target[1].value;
 
-        // Update local storage
-        localStorage.setItem('one', JSON.stringify(one));
+      // Update local storage
+      localStorage.setItem('one', JSON.stringify(one));
     });
     const formTwo = document.createElement('form');
     formTwo.addEventListener('submit', (event) => {
       event.preventDefault();
-  
+
       // Update the item's name and link
       two[i].name = event.target[0].value;
       two[i].link = event.target[1].value;
-  
+
       // Update local storage
       localStorage.setItem('two', JSON.stringify(two));
     });
     const formThree = document.createElement('form');
     formThree.addEventListener('submit', (event) => {
       event.preventDefault();
-  
+    
       // Update the item's name and link
-      three[i].name = event.target[0].value;
       three[i].link = event.target[1].value;
-  
+    
       // Update local storage
       localStorage.setItem('three', JSON.stringify(three));
     });
-  
+    
     // Create input fields for each item
     const inputNameOne = document.createElement('input');
     inputNameOne.type = 'text';
@@ -74,7 +72,7 @@ window.onload = function() {
     buttonOne.textContent = 'Submit';
     formOne.appendChild(buttonOne);
     tdOne.appendChild(formOne);
-  
+    
     const inputNameTwo = document.createElement('input');
     inputNameTwo.type = 'text';
     inputNameTwo.name = 'name';
@@ -90,7 +88,7 @@ window.onload = function() {
     buttonTwo.textContent = 'Submit';
     formTwo.appendChild(buttonTwo);
     tdTwo.appendChild(formTwo);
-  
+    
     const inputNameThree = document.createElement('input');
     inputNameThree.type = 'text';
     inputNameThree.name = 'name';
@@ -106,9 +104,118 @@ window.onload = function() {
     buttonThree.textContent = 'Submit';
     formThree.appendChild(buttonThree);
     tdThree.appendChild(formThree);
-  
+    
+    // Create a dropdown menu for each item
+    const select = document.createElement('select');
+    const optionOne = document.createElement('option');
+    optionOne.value = 'one';
+    optionOne.textContent = 'Array One';
+    const optionTwo = document.createElement('option');
+    optionTwo.value = 'two';
+    optionTwo.textContent = 'Array Two';
+    const optionThree = document.createElement('option');
+    optionThree.value = 'three';
+    optionThree.textContent = 'Array Three';
+    select.appendChild(optionOne);
+    select.appendChild(optionTwo);
+    select.appendChild(optionThree);
+
+    // Create a delete button for each item
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      // Remove the item from the selected array
+      if (select.value === 'one') {
+        one.splice(i, 1);
+        localStorage.setItem('one', JSON.stringify(one));
+      } else if (select.value === 'two') {
+        two.splice(i, 1);
+        localStorage.setItem('two', JSON.stringify(two));
+      } else if (select.value === 'three') {
+        three.splice(i, 1);
+        localStorage.setItem('three', JSON.stringify(three));
+      }
+
+      // Remove the row from the table
+      tr.remove();
+    });
+    tr.appendChild(select);
+    tr.appendChild(deleteButton);
+
+    // Add the row to the table
     table.appendChild(tr);
-  }
-  
-  container.appendChild(table);
-}
+    });
+
+    // Add the table to the container
+    container.appendChild(table);
+
+    // Add form to add a new item to a specific array
+    const addForm = document.createElement('form');
+    addForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      // Get the values from the form
+      const name = event.target[0].value;
+      const link = event.target[1].value;
+      const array = event.target[2].value;
+
+      // Add the item to the selected array
+      if (array === 'one') {
+        one.push({ name, link });
+        localStorage.setItem('one', JSON.stringify(one));
+      } else if (array === 'two') {
+        two.push({ name, link });
+        localStorage.setItem('two', JSON.stringify(two));
+      } else if (array === 'three') {
+        three.push({ name, link });
+        localStorage.setItem('three', JSON.stringify(three));
+      }
+
+      // Add a new row to the table
+      const tr = document.createElement('tr');
+      const tdOne = document.createElement('td');
+      const tdTwo = document.createElement('td');
+      const tdThree = document.createElement('td');
+      tr.appendChild(tdOne);
+      tr.appendChild(tdTwo);
+      tr.appendChild(tdThree);
+      table.appendChild(tr);
+    });
+
+    // Add input fields to the form
+    const inputName = document.createElement('input');
+    inputName.type = 'text';
+    inputName.name = 'name';
+    inputName.placeholder = 'Name';
+    addForm.appendChild(inputName);
+    const inputLink = document.createElement('input');
+    inputLink.type = 'text';
+    inputLink.name = 'link';
+    inputLink.placeholder = 'Link';
+    addForm.appendChild(inputLink);
+
+    // Add a dropdown menu to select the array
+    const addSelect = document.createElement('select');
+    const addOptionOne = document.createElement('option');
+    addOptionOne.value = 'one';
+    addOptionOne.textContent = 'Array One';
+    const addOptionTwo = document.createElement('option');
+    addOptionTwo.value = 'two';
+    addOptionTwo.textContent = 'Array Two';
+    const addOptionThree = document.createElement('option');
+    addOptionThree.value = 'three';
+    addOptionThree.textContent = 'Array Three';
+    addSelect.appendChild(addOptionOne);
+    addSelect.appendChild(addOptionTwo);
+    addSelect.appendChild(addOptionThree);
+    addForm.appendChild(addSelect);
+
+    // Add a submit button to the form
+    const addButton = document.createElement('button');
+    addButton.type = 'submit';
+    addButton.textContent = 'Add';
+    addForm.appendChild(addButton);
+
+    // Add the form to the container
+    container.appendChild(addForm);
+    };
